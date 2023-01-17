932baea00fc36de5697b93eabbbe65a64711d1f1
POO <josue.salazar@epn.edu.ec>
2023-01-17 11:48:40 -0500

    IMC

--- src/v1.java
+++ src/v1.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class v1 {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class v1 {
+    private JPanel panel;
+    private JButton SIGUIENTEButton;
+    private JFormattedTextField Peso;
+    private JTextField Altura;
+    private JLabel labelRespuesta;
+    public v1() {
+        SIGUIENTEButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               String peso = Peso.getText();
+               String altura = Altura.getText();
+               double imcpeso = Double.parseDouble(peso);
+               double imcaltura = Double.parseDouble(altura);
+               double formula=Math.pow(imcaltura,2)/imcpeso;
+               String imc = String.format("%.2f",formula);
+                labelRespuesta.setText("SU PESO ES: " + imc );
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("v1");
+        frame.setContentPane(new v1().panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
